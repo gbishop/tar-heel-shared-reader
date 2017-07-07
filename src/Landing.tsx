39cf4ef4e06c9cfca8cb84d019b31f483feb7e60
Naeim Zarei <naeimzarei@Naeims-MacBook-Pro.local>
2017-07-07 12:30:57 -0400

    updates

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -20,7 +20,8 @@ class Landing extends React.Component <LandingProps, LandingState> {
     constructor () {
         super();
         this.state = {
-            message: 'Welcome to Tar Heel Shared Reader! Please enter your email address to continue. Then, login using your Google account.',
+            message: 'Welcome to Tar Heel Shared Reader! Please enter your email address to continue. ' +
+            'Then, login using your Google account.',
             outerDivStyle: {
                 position: 'absolute',
                 display: 'inline-flex',
@@ -44,14 +45,14 @@ class Landing extends React.Component <LandingProps, LandingState> {
                 padding: '10px',
                 background: 'linear-gradient(white, #e0dede)',
                 color: '#192231',
-                fontSize: "12px"
+                fontSize: '12px'
             },
             inputStyle: {
                 width: '200px'
             },
             email: '',
             register: '',
-            mode: 0 /* Default 0 */
+            mode: 1 /* Default 0 */
         };
 
         this.handleInput = this.handleInput.bind(this);
@@ -61,12 +62,12 @@ class Landing extends React.Component <LandingProps, LandingState> {
 
     componentWillMount() {
         var config = {
-            apiKey: "AIzaSyCRHcXYbVB_eJn9Dd0BQ7whxyS2at6rkGc",
-            authDomain: "tarheelsharedreader-9f793.firebaseapp.com",
-            databaseURL: "https://tarheelsharedreader-9f793.firebaseio.com",
-            projectId: "tarheelsharedreader-9f793",
-            storageBucket: "tarheelsharedreader-9f793.appspot.com",
-            messagingSenderId: "686575466062"
+            apiKey: 'AIzaSyCRHcXYbVB_eJn9Dd0BQ7whxyS2at6rkGc',
+            authDomain: 'tarheelsharedreader-9f793.firebaseapp.com',
+            databaseURL: 'https://tarheelsharedreader-9f793.firebaseio.com',
+            projectId: 'tarheelsharedreader-9f793',
+            storageBucket: 'tarheelsharedreader-9f793.appspot.com',
+            messagingSenderId: '686575466062'
         };
         firebase.initializeApp(config);
     }
@@ -80,19 +81,20 @@ class Landing extends React.Component <LandingProps, LandingState> {
         const self = this;
         e.preventDefault();
         var tempBool = false;
-        firebase.database().ref('/registeredEmails').once('value', function(snapshot) {
-            snapshot.forEach(function(childSnapshot) {
+        firebase.database().ref('/registeredEmails').once('value', function(snapshot: any) {
+            snapshot.forEach(function(childSnapshot: any) {
                 if (self.state.email === childSnapshot.child('email').val()) {
                     self.googleSignIn();
                     tempBool = true;
                 }
                 return false;
             });
-        }).catch(function(error) {
+        }).catch(function(error: any) {
             console.log(error.message);
         }).then(function() {
             if (tempBool === false) {
-                self.setState({message: "Email does not exist in database. Please contact the web master for assistance."});
+                self.setState({message: 'Email does not exist in database. ' +
+                'Please contact the web master for assistance.'});
             }
         });
     }
@@ -100,8 +102,8 @@ class Landing extends React.Component <LandingProps, LandingState> {
     googleSignIn() {
         const self = this;
         var provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithPopup(provider).then(function(result) {
-            self.setState({message: 'Welcome, ' + result.user.email + ". Please wait while we redirect you..."});
+        firebase.auth().signInWithPopup(provider).then(function(result: any) {
+            self.setState({message: 'Welcome, ' + result.user.email + '. Please wait while we redirect you...'});
             setTimeout(function() {
                 self.setState({mode: 1});
             }, 3000);
@@ -131,10 +133,12 @@ class Landing extends React.Component <LandingProps, LandingState> {
                         </div>
                         <br/>
                         &nbsp;
-                        <input style={{position: 'relative', width: '200px', left: '1px'}} type="text" name="register" placeholder="register" value={this.state.register} onChange={this.handleInput}/>
+                        <input style={{position: 'relative', width: '200px', left: '1px'}} type="text" name="register"
+                               placeholder="register" value={this.state.register} onChange={this.handleInput}/>
                         &nbsp;
                         <button type="button" onClick={this.addEmail}>Register</button> <br/>
-                        <input style={this.state.inputStyle} type="text" name="email" placeholder="email" value={this.state.email} onChange={this.handleInput}/>
+                        <input style={this.state.inputStyle} type="text" name="email" placeholder="email"
+                               value={this.state.email} onChange={this.handleInput}/>
                         &nbsp;
                         <button type="button" onClick={this.validate}>Validate</button>
                     </div>
@@ -155,7 +159,12 @@ interface ClassRollState {
     lastName: string;
     registerStudentStyle: any;
     isRegisterHidden: boolean;
+    isUpdateHidden: boolean;
+    isRemoveHidden: boolean;
     tableCellsArray: any[];
+    checkedSelection: any;
+    defaultFirstName: string;
+    defaultLastName: string;
 }
 
 class ClassRoll extends React.Component<ClassRollProps, ClassRollState> {
@@ -163,21 +172,21 @@ class ClassRoll extends React.Component<ClassRollProps, ClassRollState> {
         super();
         this.state = {
             outerDivStyle: {
-                fontFamily: "Didot",
-                position: "absolute",
-                width: "750px",
-                height: "600px",
-                background: "linear-gradient(white, #8e8e8e)",
-                display: "inline-flex",
+                fontFamily: 'Didot',
+                position: 'absolute',
+                width: '750px',
+                height: '600px',
+                background: 'linear-gradient(white, #8e8e8e)',
+                display: 'inline-flex',
                 left: '50%',
                 top: '50%',
                 marginLeft: '-375px',
                 marginTop: '-300px',
-                borderRadius: "25px",
-                userSelect: "none"
+                borderRadius: '25px',
+                userSelect: 'none'
             },
             registerStudentStyle: {
-                fontFamily: "Didot",
+                fontFamily: 'Didot',
                 position: 'fixed',
                 width: '500px',
                 height: '55px',
@@ -186,7 +195,7 @@ class ClassRoll extends React.Component<ClassRollProps, ClassRollState> {
                 marginTop: '-27.5px',
                 left: '50%',
                 top: '50%',
-                zIndex: "1",
+                zIndex: '1',
                 borderRadius: '5px',
                 color: 'black',
                 textAlign: 'center',
@@ -196,7 +205,12 @@ class ClassRoll extends React.Component<ClassRollProps, ClassRollState> {
             firstName: '',
             lastName: '',
             isRegisterHidden: true,
-            tableCellsArray: []
+            isUpdateHidden: true,
+            isRemoveHidden: true,
+            tableCellsArray: [],
+            checkedSelection: '',
+            defaultFirstName: '',
+            defaultLastName: ''
         };
 
         this.handleBlur = this.handleBlur.bind(this);
@@ -205,6 +219,7 @@ class ClassRoll extends React.Component<ClassRollProps, ClassRollState> {
         this.handleInput = this.handleInput.bind(this);
         this.updateStudent = this.updateStudent.bind(this);
         this.checkSelection = this.checkSelection.bind(this);
+        this.closeWindow = this.closeWindow.bind(this);
     }
 
     componentWillMount() {
@@ -241,7 +256,7 @@ class ClassRoll extends React.Component<ClassRollProps, ClassRollState> {
                 newArr.push(student);
                 self.setState({tableCellsArray: newArr});
             });
-        }, 350);
+        }, 500);
     }
 
     getUserID() {
@@ -256,41 +271,66 @@ class ClassRoll extends React.Component<ClassRollProps, ClassRollState> {
 
     handleBlur(e: any) {
         e.preventDefault();
-        this.setState({isRegisterHidden: !this.state.isRegisterHidden});
+        if (this.state.isRegisterHidden === false || this.state.isUpdateHidden === false ||
+            this.state.isRemoveHidden === false) {
+            return;
+        }
+
+        if (e.target.innerHTML === 'Add Student') {
+            this.setState({isRegisterHidden: !this.state.isRegisterHidden});
+        } else if (e.target.innerHTML === 'Remove Student') {
+            this.setState({isRemoveHidden: !this.state.isRemoveHidden});
+        } else if (e.target.innerHTML === 'Update Student') {
+            if (this.state.checkedSelection === '') {
+                alert('Please select a student first.');
+                return;
+            }
+            this.setState({
+                isUpdateHidden: !this.state.isUpdateHidden,
+                defaultFirstName: this.state.checkedSelection.childNodes[1].innerHTML,
+                defaultLastName: this.state.checkedSelection.childNodes[2].innerHTML
+            });
+        }
         this.setState({
             outerDivStyle: {
-                position: "absolute",
-                width: "750px",
-                height: "600px",
-                background: "linear-gradient(white, #8e8e8e)",
-                display: "inline-flex",
+                position: 'absolute',
+                width: '750px',
+                height: '600px',
+                background: 'linear-gradient(white, #8e8e8e)',
+                display: 'inline-flex',
                 left: '50%',
                 top: '50%',
                 marginLeft: '-375px',
                 marginTop: '-300px',
                 borderRadius: '25px',
                 userSelect: 'none',
-                filter: (this.state.outerDivStyle.filter === 'blur(5px)') ? 'blur(0px)' : 'blur(5px)'
+                filter: (this.state.outerDivStyle.filter === 'blur(10px)') ? 'blur(0px)' : 'blur(10px)'
             }
         });
     }
 
     closeWindow() {
-        this.setState({isRegisterHidden: !this.state.isRegisterHidden});
+        if (this.state.isRegisterHidden === false) {
+            this.setState({isRegisterHidden: !this.state.isRegisterHidden});
+        } else if (this.state.isUpdateHidden === false) {
+            this.setState({isUpdateHidden: !this.state.isUpdateHidden});
+        } else if (this.state.isRemoveHidden === false) {
+            this.setState({isRemoveHidden: !this.state.isRemoveHidden});
+        }
         this.setState({
             outerDivStyle: {
-                position: "absolute",
-                width: "750px",
-                height: "600px",
-                background: "linear-gradient(white, #8e8e8e)",
-                display: "inline-flex",
+                position: 'absolute',
+                width: '750px',
+                height: '600px',
+                background: 'linear-gradient(white, #8e8e8e)',
+                display: 'inline-flex',
                 left: '50%',
                 top: '50%',
                 marginLeft: '-375px',
                 marginTop: '-300px',
                 borderRadius: '25px',
                 userSelect: 'none',
-                filter: (this.state.outerDivStyle.filter === 'blur(5px)') ? 'blur(0px)' : 'blur(5px)'
+                filter: (this.state.outerDivStyle.filter === 'blur(10px)') ? 'blur(0px)' : 'blur(10px)'
             }
         });
     }
@@ -309,20 +349,20 @@ class ClassRoll extends React.Component<ClassRollProps, ClassRollState> {
         newRef.set({
             firstName: self.state.firstName,
             lastName: self.state.lastName
-        }).catch(function(error) {
+        }).catch(function(error: any) {
             console.log(error.message);
         }).then(function() {
             self.setState({firstName: '', lastName: ''});
             self.closeWindow();
-        })
+        });
     }
 
     removeStudent(e: any) {
-
+        e.preventDefault();
     }
 
     updateStudent(e: any) {
-
+        e.preventDefault();
     }
 
     handleInput(e: any) {
@@ -330,10 +370,30 @@ class ClassRoll extends React.Component<ClassRollProps, ClassRollState> {
         this.setState({[e.target.name]: e.target.value});
     }
 
+    activate(e: any) {
+        console.log('activate');
+    }
+
     checkSelection(e: any) {
         e.preventDefault();
-        // TODO
-        e.target.parentElement.style.backgroundColor = "white";
+        if (e.target.parentElement.childNodes[0].tagName === 'TH') {
+            return;
+        }
+
+        let originalTarget = this.state.checkedSelection;
+        let newTarget = e.target.parentElement;
+
+        if (originalTarget !== '' && newTarget !== originalTarget) {
+            return;
+        }
+
+        if (e.target.parentElement.style.backgroundColor !== 'white') {
+            e.target.parentElement.style.backgroundColor = 'white';
+            this.setState({checkedSelection: e.target.parentElement});
+        } else {
+            e.target.parentElement.style.backgroundColor = 'transparent';
+            this.setState({checkedSelection: ''});
+        }
     }
 
     render() {
@@ -342,9 +402,14 @@ class ClassRoll extends React.Component<ClassRollProps, ClassRollState> {
                 <div style={this.state.outerDivStyle}>
                     <table className="register-button">
                         <tr>
-                            <td><button className="student-button add-student" type="text" onClick={this.handleBlur}>Add Student</button></td>
-                            <td><button className="student-button remove-student" type="text" onClick={this.removeStudent}>Remove Student</button></td>
-                            <td><button className="student-button update-student" type="text" onClick={this.updateStudent}>Update Student</button></td>
+                            <td><button className="student-button add-student" type="text"
+                                        onClick={this.handleBlur}>Add Student</button></td>
+                            <td><button className="student-button remove-student" type="text"
+                                        onClick={this.handleBlur}>Remove Student</button></td>
+                            <td><button className="student-button update-student" type="text"
+                                        onClick={this.handleBlur}>Update Student</button></td>
+                            <td><button className="student-button update-student" type="text"
+                                        onClick={this.activate}>Activate Student</button></td>
                         </tr>
                     </table>
                     <table className="student-table" onClick={this.checkSelection}>
@@ -364,7 +429,17 @@ class ClassRoll extends React.Component<ClassRollProps, ClassRollState> {
                                       onChange={this.handleInput} placeholder="Last Name"/><br/><br/>
                     <button type="button" onClick={this.addStudent}>Add Student</button>
                     &nbsp;
-                    <button type="button" onClick={this.handleBlur}>Close</button>
+                    <button type="button" onClick={this.closeWindow}>Close</button>
+                </div>
+                <div style={this.state.registerStudentStyle} hidden={this.state.isUpdateHidden}>
+                    First Name: <input type="text" name="firstName" value={this.state.defaultFirstName}
+                                       onChange={this.handleInput}/>
+                    &nbsp;
+                    Last Name: <input type="text" name="lastName" value={this.state.defaultLastName}
+                                      onChange={this.handleInput}/><br/>
+                    <button type="button" onClick={this.updateStudent}>Update Student</button>
+                    &nbsp;
+                    <button type="button" onClick={this.closeWindow}>Close</button>
                 </div>
             </div>
         );
