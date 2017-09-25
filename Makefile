b4c81ffc6a765d8fbb76948eff3592afe586f9f1
Gary Bishop <gary.bishop.unc@gmail.com>
2017-09-25 14:06:12 -0400

    fixes by Naeim and Gary

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 deploy:
 	npm run build
 	cp -a ../symbols.dynavox/* build/symbols
-	rsync -a --delete-after build/ gbserver3:/var/www/shared.tarheelreader/
+	rsync -a build/ gbserver3:/var/www/shared.tarheelreader/
--- src/activate-email.py
+++ src/activate-email.py
@@ -29,8 +29,8 @@ def getFirebaseRef(path):
 @app.hook('after_request')
 def enable_cors():
     """Enable CORS on Bottle.py"""
-    response.headers['Access-Control-Allow-Origin'] = '*';
-    response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Authorization, Origin, Accept, Content-Type, X-Requested-With'
 
 @app.route('/hello', method=['OPTIONS', 'GET'])
@@ -44,23 +44,25 @@ def hello():
             'id': 410
         }
         
-@app.route('/activate', method=['OPTIONS', 'POST'])
+@app.route('/activate', method='OPTIONS')
 def activate():
     """Send an activation request"""
-    if request.method == 'OPTIONS':
-        return {}
-    else:
-        teacherID = request.json['teacherID']
-        email = request.json['email']
-        active = False
-        
-        with open('emails.json') as data_file:
-            data = json.load(data_file)
-            verifiedEmails = data['emails']
-            if email in verifiedEmails:
-                active = True
-                getFirebaseRef('/users/admin/' + teacherID).set({'email': email, 'active': True})
+    return {}
+
+@app.route('/activate', method='POST')
+def activate():
+    """Send an activation request"""
+    teacherID = request.json['teacherID']
+    email = request.json['email']
+    active = False
     
-        return {'active': active}
+    with open('emails.json') as data_file:
+        data = json.load(data_file)
+        verifiedEmails = data['emails']
+        if email in verifiedEmails:
+            active = True
+            getFirebaseRef('/users/admin/' + teacherID).set({'email': email, 'active': True})
+
+    return {'active': active}
 
 run(app, host='localhost', port=8080)
