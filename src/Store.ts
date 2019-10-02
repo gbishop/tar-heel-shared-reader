e32a8585931814c9ae573fbe686e4818fa7f90ad
Gary Bishop <gary.bishop.unc@gmail.com>
2019-10-02 14:20:34 -0400

    Fix bugs in response buttons

--- src/Store.ts
+++ src/Store.ts
@@ -1,23 +1,55 @@
-import { observable, computed, action, ObservableMap } from 'mobx';
-import { DB, LogRecord } from './db';
+import { observable, computed, action, ObservableMap } from "mobx";
+import { DB, LogRecord } from "./db";
 
 export const allResponses: string[] = [
-  'like', 'want', 'not', 'go',
-  'get', 'make', 'look', 'turn',
-  'good', 'more', 'help', 'different',
-  'I', 'he', 'you', 'she',
-  'open', 'do', 'that', 'up',
-  'put', 'same', 'all', 'some',
-  'it', 'here', 'where', 'what',
-  'in', 'on', 'why', 'who',
-  'can', 'finished', 'when', 'stop'
+  "like",
+  "want",
+  "not",
+  "go",
+  "get",
+  "make",
+  "look",
+  "turn",
+  "good",
+  "more",
+  "help",
+  "different",
+  "I",
+  "he",
+  "you",
+  "she",
+  "open",
+  "do",
+  "that",
+  "up",
+  "put",
+  "same",
+  "all",
+  "some",
+  "it",
+  "here",
+  "where",
+  "what",
+  "in",
+  "on",
+  "why",
+  "who",
+  "can",
+  "finished",
+  "when",
+  "stop"
 ];
+
+// allow changing persistence data
+const PERSISTVERSION = 1;
 
 class Store {
   public db: DB;
 
-  // does user have admin privileges 
-  @computed get isAdmin() { return this.db.isAdmin; }
+  // does user have admin privileges
+  @computed get isAdmin() {
+    return this.db.isAdmin;
+  }
 
   // thr login name
   @computed get teacherid() {
@@ -26,17 +58,16 @@ class Store {
 
   // editing state
   @observable editing = false;
-  @observable editSlug = '';
+  @observable editSlug = "";
 
-  @observable studentid: string = '';
+  @observable studentid: string = "";
   // set student's id
   @action.bound setstudentid(id: string) {
     this.studentid = id;
   }
   // list of books to display
-  @computed({keepAlive: true}) get sharedBookListP() {
-    console.log('get sbl');
-    return this.db.fetchBookList('');
+  @computed({ keepAlive: true }) get sharedBookListP() {
+    return this.db.fetchBookList("");
   }
   // state of booklist display
   @observable bookListOpen: ObservableMap<string, boolean> = observable.map();
@@ -45,12 +76,11 @@ class Store {
   }
   // list of recent books for this teacher
   @computed get teacherBookListP() {
-    console.log('get rbl');
     return this.db.fetchBookList(this.teacherid);
   }
 
   // the id of the book to read or '' for the landing page
-  @observable bookid: string = '';
+  @observable bookid: string = "";
   @action.bound setBookid(s: string) {
     this.bookid = s;
     this.pageno = 1;
@@ -67,8 +97,8 @@ class Store {
     return this.bookP.case({
       rejected: () => 0,
       pending: () => 0,
-      fulfilled: (book) => book.pages.length
-    })
+      fulfilled: book => book.pages.length
+    });
   }
   // update the state typically from a URL
   @action.bound setPath(studentid: string, bookid: string, page: number) {
@@ -80,26 +110,30 @@ class Store {
   @action.bound setEditPath(slug: string) {
     this.editing = true;
     this.editSlug = slug;
-    console.log('sep', this.editing, this.editSlug);
   }
 
   // map the state to a url
   @computed get currentPath() {
     if (this.editing) {
-      const p = '/edit' + (this.editSlug ? '/' + this.editSlug : '');
-      console.log(p);
+      const p = "/edit" + (this.editSlug ? "/" + this.editSlug : "");
       return p;
     }
     if (!this.studentid) {
-      return '/';
+      return "/";
     }
-    return `/read/${encodeURIComponent(this.studentid)}/${this.bookid}` + (this.pageno > 1 ? `/${this.pageno}` : '');
+    return (
+      `/read/${encodeURIComponent(this.studentid)}/${this.bookid}` +
+      (this.pageno > 1 ? `/${this.pageno}` : "")
+    );
   }
   // set the page number
   @action.bound public setPage(i: number) {
     this.pageno = i;
-    if (this.bookP.state === 'fulfilled') {
-      this.pageno = Math.max(1, Math.min(this.bookP.value.pages.length+1, this.pageno));
+    if (this.bookP.state === "fulfilled") {
+      this.pageno = Math.max(
+        1,
+        Math.min(this.bookP.value.pages.length + 1, this.pageno)
+      );
     }
   }
   // index to the readings array
@@ -112,8 +146,8 @@ class Store {
     return this.bookP.case({
       rejected: () => 0,
       pending: () => 0,
-      fulfilled: (book) => book.comments.length
-    })
+      fulfilled: book => book.comments.length
+    });
   }
   // allow excluding responses from the list
   @observable public responsesExcluded = new Map<string, boolean>();
@@ -125,25 +159,32 @@ class Store {
   @observable public responsesPerPage = 4;
   @action.bound setResponsesPerPage(n: number) {
     this.responsesPerPage = n;
+    this.responseOffset = 0;
+    this.responseIndex = -1;
   }
 
   @computed get allowedResponses() {
-    return allResponses.filter(r => !this.responsesExcluded.get(r));
+    const a = allResponses.filter(r => !this.responsesExcluded.get(r));
+    return a;
   }
   // get responses for this reading
   @computed get responses() {
-    return this.allowedResponses.slice(this.responseOffset, this.responseOffset + this.responsesPerPage);
+    const r = this.allowedResponses.slice(
+      this.responseOffset,
+      this.responseOffset + this.responsesPerPage
+    );
+    return r;
   }
   @action.bound public stepResponsePage(direction: number) {
-    const rpp = this.responsesPerPage,
-          pageNo = Math.floor(this.responseOffset / rpp),
-          N = this.allowedResponses.length;
-    this.responseOffset = (((pageNo + direction) * rpp) % N + N) % N;
+    const N = this.allowedResponses.length,
+      rpp = Math.min(N, this.responsesPerPage),
+      pageNo = Math.floor(this.responseOffset / rpp);
+    this.responseOffset = ((((pageNo + direction) * rpp) % N) + N) % N;
     this.responseIndex = -1;
   }
 
   // placement of the response symbols
-  @observable public layout: string = 'bottom';
+  @observable public layout: string = "bottom";
   @action.bound public setLayout(side: string) {
     this.layout = side;
   }
@@ -156,7 +197,9 @@ class Store {
 
   // currently selected response symbol
   @observable public responseIndex: number = -1;
-  @computed get nresponses() { return this.responses.length; }
+  @computed get nresponses() {
+    return this.responses.length;
+  }
   @action.bound public nextResponseIndex() {
     this.responseIndex = (this.responseIndex + 1) % this.nresponses;
   }
@@ -164,7 +207,9 @@ class Store {
     this.responseIndex = i;
   }
   // current response
-  @computed get word() { return this.responses[this.responseIndex]; }
+  @computed get word() {
+    return this.responses[this.responseIndex];
+  }
 
   // visibility of the controls modal
   @observable public controlsVisible: boolean = false;
@@ -188,7 +233,10 @@ class Store {
   // json string to persist the state
   @computed get persist(): string {
     const obj = {
+      version: PERSISTVERSION,
       layout: this.layout,
+      responsesPerPage: this.responsesPerPage,
+      responses: this.allowedResponses,
       responseSize: this.responseSize,
       pageTurnVisible: this.pageTurnVisible,
       bookListOpen: this.bookListOpen.toJSON()
@@ -199,10 +247,16 @@ class Store {
   // restore the state from json
   @action.bound public setPersist(js: string) {
     const v = JSON.parse(js);
-    this.layout = v.layout;
-    this.responseSize = v.responseSize;
-    this.pageTurnVisible = v.pageTurnVisible;
-    Object.keys(v.bookListOpen).forEach(key => this.bookListOpen.set(key, v.bookListOpen[key]));
+    if (v && "version" in v && v.version === PERSISTVERSION) {
+      this.layout = v.layout;
+      this.responsesPerPage = v.responsesPerPage;
+      allResponses.map(r => this.setExcluded(r, v.responses.indexOf(r) < 0));
+      this.responseSize = v.responseSize;
+      this.pageTurnVisible = v.pageTurnVisible;
+      Object.keys(v.bookListOpen).forEach(key =>
+        this.bookListOpen.set(key, v.bookListOpen[key])
+      );
+    }
   }
 
   // log state changes
